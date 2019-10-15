b244e530d4d636a4586752f96d202479bbc9a4a8
jfs <janfrantiseksedlacek@gmail.com>
2019-10-15 20:41:25 +0200

    Init

---
+++ src/UI/UI.java
@@ -0,0 +1,31 @@
+package UI;
+
+/**
+ * UI
+ *
+ * @author Jan Frantisek Sedlacek
+ * @version 1.0
+ * @since 15.10.19
+ */
+public class UI {
+
+// =========== CONSTANTS ===========
+
+// =========== /CONSTANTS ===========
+
+// =========== VARIABLES ===========
+
+// =========== /VARIABLES ===========
+
+// =========== CONSTRUCTOR ===========
+
+// =========== /CONSTRUCTOR ===========
+
+// =========== SPECIAL METHODS ===========
+
+// =========== /SPECIAL METHODS ===========
+
+// =========== GETTERS & SETTERS ===========
+
+// =========== /GETTERS & SETTERS ===========
+}
